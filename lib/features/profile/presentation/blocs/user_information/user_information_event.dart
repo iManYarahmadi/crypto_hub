04f72d79b27aaa1da3f5ac38a5f06c34f1bed741
iManYarahmadi <i.yarahmadi@yahoo.com>
2025-03-07 02:29:37 +0330

    update(crypto_hub|feature|profile):Impl Presentation Layer --impl bloc classes

---
+++ lib/features/profile/presentation/blocs/user_information/user_information_event.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_information_event.freezed.dart';
+
+@freezed
+class UserInformationEvent with _$UserInformationEvent {
+  const factory UserInformationEvent.fetchUserInformation() = _FetchUserInformation;
+}
---
+++ lib/features/profile/presentation/blocs/user_information/user_information_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:cryptohub/features/profile/domain/entities/user_information_entity.dart';
+
+part 'user_information_state.freezed.dart';
+
+@freezed
+class UserInformationState with _$UserInformationState {
+  const factory UserInformationState.initial() = _Initial;
+  const factory UserInformationState.loading() = _Loading;
+  const factory UserInformationState.loaded(UserInformationEntity userInfo) = _Loaded;
+  const factory UserInformationState.error(String message) = _Error;
+}
