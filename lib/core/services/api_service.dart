e344625f7e8caf41df60d5fc5343cc93e24989fd
iManYarahmadi <i.yarahmadi@yahoo.com>
2025-03-06 20:25:35 +0330

    refactor(crypto_hub):Refactor Api Service To Handle Option On Request

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -2,6 +2,8 @@ import 'package:cryptohub/core/error/exceptions.dart' show ServerException;
 import 'package:cryptohub/core/services/logging_interceptor.dart';
 import 'package:dio/dio.dart';
 
+import 'package:cryptohub/core/error/exceptions.dart';
+
 class ApiService {
   final Dio dio;
 
@@ -10,21 +12,28 @@ class ApiService {
     dio.options.connectTimeout = Duration(seconds: 10);
     dio.options.receiveTimeout = Duration(seconds: 10);
     dio.options.headers = {
-      'accept': 'application/json', // اضافه کردن هدر accept
-      'Content-Type': 'application/json', // هدر Content-Type
+      'accept': 'application/json',
+      'Content-Type': 'application/json',
     };
-
     dio.interceptors.add(LoggingInterceptor());
   }
 
-  Future<Response> post(String path, Map<String, dynamic> data) async {
+  Future<Response> post(
+      String path,
+      Map<String, dynamic> data, {
+        Options? options,
+      }) async {
     try {
-      final response = await dio.post(path, data: data);
+      final response = await dio.post(
+        path,
+        data: data,
+        options: options ?? Options(),
+      );
       return response;
     } on DioException catch (e) {
-      throw ServerException('خطای سرور: ${e.message} - ${e.response?.statusCode}');
+      throw ServerException('Server error: ${e.message} - ${e.response?.statusCode}');
     } catch (e) {
-      throw ServerException('خطای ناشناخته: $e');
+      throw ServerException('Unknown error: $e');
     }
   }
 }
